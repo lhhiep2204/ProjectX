bfbbf4ad5e0f88cabb6199b0e08ad265b3a8df68
Hiệp Lê <lhhiep2204@gmail.com>
2024-03-30 22:46:01 +0700

    [Utilities] Remove KeychainAccess library

--- Modules/Utilities/Utilities/KeychainHelper.swift
+++ Modules/Utilities/Utilities/KeychainHelper.swift
@@ -5,15 +5,17 @@
 //  Created by Hoàng Hiệp Lê on 16/03/2024.
 //
 
+// MARK: - Uncomment below snippets and add KeychainAccess library to Swift Package Manager when you want to use Keychain
+/*
 import KeychainAccess
 
 public class KeychainHelper {
     public static let shared = KeychainHelper()
-    
+
     private init() {}
 
-    private let keychain = Keychain(service: "com.hieplee.HHEverything")
-    
+    private let keychain = Keychain(service: "com.hieplee.ProjectX")
+
     /// Retrieves a value from the keychain for the specified key.
     ///
     /// - Parameter key: The key associated with the value in the keychain.
@@ -22,11 +24,11 @@ public class KeychainHelper {
         do {
             return try keychain.get(key)
         } catch {
-            print("Error retrieving data from Keychain for key \(key): \(error)")
+            Logger.error("Error retrieving data from Keychain for key \(key): \(error)")
             return nil
         }
     }
-    
+
     /// Sets a value in the keychain for the specified key.
     ///
     /// - Parameters:
@@ -34,14 +36,50 @@ public class KeychainHelper {
     ///   - key: The key associated with the value in the keychain.
     public func set(_ value: String, forKey key: String) {
         do {
-            try keychain
-                .accessibility(.whenUnlockedThisDeviceOnly, authenticationPolicy: .userPresence)
-                .set(value, key: key)
+            try keychain.set(value, key: key)
         } catch {
-            print("Error setting data in Keychain for key \(key): \(error)")
+            Logger.error("Error setting data in Keychain for key \(key): \(error)")
         }
     }
-    
+
+    /// Retrieves a Codable value from the keychain for the specified key.
+    ///
+    /// - Parameters:
+    ///   - type: The type of the Codable value to be retrieved.
+    ///   - key: The key associated with the Codable value in the keychain.
+    /// - Returns: The Codable value associated with the key, or `nil` if not found.
+    public func get<T: Codable>(type: T.Type, forKey key: String) -> T? {
+        do {
+            guard let data = try keychain.getData(key) else {
+                return nil
+            }
+
+            let decoder = JSONDecoder()
+            let value = try decoder.decode(type, from: data)
+
+            return value
+        } catch {
+            Logger.error("Error retrieving data from Keychain for key \(key): \(error)")
+            return nil
+        }
+    }
+
+    /// Sets a Codable value in the keychain for the specified key.
+    ///
+    /// - Parameters:
+    ///   - value: The Codable value to be stored in the keychain.
+    ///   - key: The key associated with the Codable value in the keychain.
+    public func set<T: Codable>(_ value: T, forKey key: String) {
+        do {
+            let encoder = JSONEncoder()
+            let data = try encoder.encode(value)
+
+            try keychain.set(data, key: key)
+        } catch {
+            Logger.error("Error retrieving data from Keychain for key \(key): \(error)")
+        }
+    }
+
     /// Removes a value from the keychain for the specified key.
     ///
     /// - Parameter key: The key associated with the value to be deleted.
@@ -49,7 +87,8 @@ public class KeychainHelper {
         do {
             try keychain.remove(key)
         } catch let error {
-            print("Error removing data from Keychain for key \(key): \(error)")
+            Logger.error("Error removing data from Keychain for key \(key): \(error)")
         }
     }
 }
+*/
